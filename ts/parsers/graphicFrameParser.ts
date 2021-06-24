0c13073ffc315362aa143f0cc8648f6051d960a8
rrabii <rabi.ulislam92@gmail.com>
2021-06-24 17:38:04 +0400

    refactor: add line break above switch

--- ts/parsers/graphicFrameParser.ts
+++ ts/parsers/graphicFrameParser.ts
@@ -7,6 +7,7 @@ export default class GraphicFrameParser {
     const result = [];
     for (const frame of graphicFrames) {
       const graphicTypeUri = getAttributeByPath([frame], ["a:graphic", "a:graphicData", "$", "uri"]);
+
       switch (graphicTypeUri) {
         case "http://schemas.openxmlformats.org/drawingml/2006/table":
           result.push(frame);
