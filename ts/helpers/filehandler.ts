e9bbb41cbb77c978d37a57b8ca661b235ee9abd3
rrabii <rabi.ulislam92@gmail.com>
2021-08-26 15:41:18 +0400

    chore: remove logs

--- ts/helpers/filehandler.ts
+++ ts/helpers/filehandler.ts
@@ -4,9 +4,7 @@ import * as xml2js from "xml2js-es6-promise";
 
 export default class FileHandler {
     public static async parseSlideAttributes(fileName) {
-        console.log(fileName)
         const presentationSlide = await fs.readFile(fileName, 'utf8');
-        console.log(presentationSlide)
         const parsedPresentationSlide = await xml2js(presentationSlide, {
             trim: true,
             preserveChildrenOrderForMixedContent: true
