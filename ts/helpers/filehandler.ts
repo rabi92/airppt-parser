ebc49803651882c1f2764672604af02cf506f579
Rabi <rabi.ulislam92@gmail.com>
2021-08-31 12:57:22 +0400

    refactor: remove assigning to a variable

--- ts/helpers/filehandler.ts
+++ ts/helpers/filehandler.ts
@@ -3,8 +3,7 @@ import * as xml2js from "xml2js-es6-promise";
 
 export default class FileHandler {
     public static async parseContentFromFile(fileName) {
-        const presentationXML = await fs.readFile(fileName, "utf8");
-        return xml2js(presentationXML, {
+        return await xml2js(await fs.readFile(fileName, "utf8"), {
             trim: true,
             preserveChildrenOrderForMixedContent: true
         });
