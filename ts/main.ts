dd0c0a63d8ed7f480f32489afd19dc5a27974e5c
Umar Ilyas <uzilyas.s3@gmail.com>
2022-09-26 14:52:09 +0400

    feat: add support for screenshots
    Introducing feature which will help to take just screenshots of slides. Function takes one parameter for considering only screenshots or not.

--- ts/main.ts
+++ ts/main.ts
@@ -3,39 +3,41 @@ import { PowerpointDetails } from "airppt-models-plus/pptdetails";
 import { PowerpointElementParser, PptGlobalsParser, SlideParser } from "./parsers";
 
 export class AirParser {
-    constructor(private readonly PowerpointFilePath: string) {}
+  constructor(private readonly PowerpointFilePath: string) {}
 
-    public async ParsePowerPoint(): Promise<PowerpointDetails> {
-        return new Promise<PowerpointDetails>(async (resolve, reject) => {
-            try {
-                const pptElementParser = new PowerpointElementParser();
-                const slidesLength = await PptGlobalsParser.getSlidesLength(this.PowerpointFilePath);
-                const slidesSections = await PptGlobalsParser.getSections(this.PowerpointFilePath);
-                const allSlides = [];
+  public async ParsePowerPoint({ isScreenshotsOnly }: { isScreenshotsOnly?: boolean }): Promise<PowerpointDetails> {
+    return new Promise<PowerpointDetails>(async (resolve, reject) => {
+      try {
+        const slidesLength = await PptGlobalsParser.getSlidesLength(this.PowerpointFilePath);
+        const slidesSections = await PptGlobalsParser.getSections(this.PowerpointFilePath);
+        const allSlides = [];
 
-                for (let i = 1; i <= slidesLength; i++) {
-                    allSlides.push(SlideParser.getSlideElements(pptElementParser, i, this.PowerpointFilePath));
-                }
+        if (!isScreenshotsOnly) {
+          const pptElementParser = new PowerpointElementParser();
+          for (let i = 1; i <= slidesLength; i++) {
+            allSlides.push(SlideParser.getSlideElements(pptElementParser, i, this.PowerpointFilePath));
+          }
+        }
 
-                Promise.allSettled(allSlides).then((result) => {
-                    const pptElements = result.map((slideElements) => {
-                        if (slideElements.status === "fulfilled") {
-                            return slideElements.value;
-                        }
-
-                        return [];
-                    });
-
-                    resolve({
-                        powerPointElements: pptElements,
-                        inputPath: this.PowerpointFilePath,
-                        sections: slidesSections,
-                        slidesLength
-                    });
-                });
-            } catch (error) {
-                reject(error)
+        Promise.allSettled(allSlides).then((result) => {
+          const pptElements = result.map((slideElements) => {
+            if (slideElements.status === "fulfilled") {
+              return slideElements.value;
             }
+
+            return [];
+          });
+
+          resolve({
+            powerPointElements: pptElements,
+            inputPath: this.PowerpointFilePath,
+            sections: slidesSections,
+            slidesLength,
+          });
         });
-    }
+      } catch (error) {
+        reject(error);
+      }
+    });
+  }
 }
